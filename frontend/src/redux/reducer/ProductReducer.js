67ae9c75026107a990209da3cd228fb99b667286
Huy Nguyen <huybka1996@gmail.com>
2022-07-02 15:55:42 +0700

    Remove from cart

--- frontend/src/redux/reducer/ProductReducer.js
+++ frontend/src/redux/reducer/ProductReducer.js
@@ -1,31 +1,30 @@
-import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/ProductContants";
-import { PRODUCT_DETAIL_FAIL,PRODUCT_DETAIL_REQUEST,PRODUCT_DETAIL_SUCCESS } from "../constants/ProductContants";
+import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from '../constants/ProductContants';
+import { PRODUCT_DETAIL_FAIL, PRODUCT_DETAIL_REQUEST, PRODUCT_DETAIL_SUCCESS } from '../constants/ProductContants';
 //GET ALL PRODUCT
-export const productListReducer = (state ={products:[]},action)=>{
-    switch(action.type){
+export const productListReducer = (state = { products: [] }, action) => {
+    console.log(`product ${action.type}`);
+    switch (action.type) {
         case PRODUCT_LIST_REQUEST:
-            return {loading: true,products:[]};
+            return { loading: true, products: [] };
         case PRODUCT_LIST_SUCCESS:
-            return {loading: false,products:action.payload};
+            return { loading: false, products: action.payload };
         case PRODUCT_LIST_FAIL:
-            return {loading: false,error:action.payload};
+            return { loading: false, error: action.payload };
         default:
             return state;
     }
-}
+};
 
 // SINGLE PRODUCT
-export const productDetailsReducer = (
-    state ={product:{reviews:[]}},
-    action)=>{
-    switch(action.type){
+export const productDetailsReducer = (state = { product: { reviews: [] } }, action) => {
+    switch (action.type) {
         case PRODUCT_DETAIL_REQUEST:
-            return {loading: true,product:[]};
+            return { loading: true, product: [] };
         case PRODUCT_DETAIL_SUCCESS:
-            return {loading: false,product:action.payload};
+            return { loading: false, product: action.payload };
         case PRODUCT_DETAIL_FAIL:
-            return {loading: false,error:action.payload};
+            return { loading: false, error: action.payload };
         default:
             return state;
     }
-}
+};
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,12 +2,21 @@ import {createStore,combineReducers, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import { productListReducer,productDetailsReducer } from './reducer/ProductReducer.js';
+import { cartReducer } from './reducer/CartReducer';
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails:productDetailsReducer,
+    cart: cartReducer,
 });
-const initialState ={}
+const cartItemsFromLocalStorage = localStorage.getItem('cartItems') 
+    ? JSON.parse(localStorage.getItem('cartItems'))
+    :[]
+const initialState ={
+    cart:{
+        cartItems: cartItemsFromLocalStorage,
+    },
+}
 const middleware = [thunk]
 
 const store = createStore(
